1daabeda7c59a4e4d372a918f100811b6eac32df
Kirill Khramkov <hramkir@gmail.com>
2022-11-10 12:40:30 +0400

    New working version

---
+++ src/lib.rs
@@ -0,0 +1,165 @@
+use std::iter;
+
+#[derive(Clone, Debug, Copy)]
+pub enum Commands {
+    Forward,
+    Back,
+    Increment,
+    Decrement,
+    Output,
+    Input,
+    LoopStart,
+    LoopEnd,
+}
+
+pub struct Interpreter {
+    memory: Vec<u8>,
+    size: usize,
+    commands: Vec<Option<Commands>>,
+    input: String,
+    output: String,
+    pointer: usize,
+    command_pointer: usize,
+}
+
+impl Interpreter {
+    pub fn new(size: usize) -> Interpreter {
+        Interpreter {
+            memory: iter::repeat(0 as u8).take(size).collect(),
+            size: size,
+            commands: Vec::new(),
+            input: String::new(),
+            output: String::new(),
+            pointer: 0,
+            command_pointer: 0,
+        }
+    }
+
+    fn parse_to_commands(&mut self, commands_string: String) -> () {
+        self.commands = commands_string
+            .chars()
+            .map(|ch| -> Option<Commands> {
+                match ch {
+                    '+' => Some(Commands::Increment),
+                    '-' => Some(Commands::Decrement),
+                    '>' => Some(Commands::Forward),
+                    '<' => Some(Commands::Back),
+                    '.' => Some(Commands::Output),
+                    ',' => Some(Commands::Input),
+                    '[' => Some(Commands::LoopStart),
+                    ']' => Some(Commands::LoopEnd),
+                    _ => None,
+                }
+            })
+            .collect();
+    }
+
+    fn get_input(&mut self, input: String) -> () {
+        self.input = input.chars().rev().collect::<String>();
+    }
+
+    pub fn get_output(self) -> String {
+        self.output
+    }
+
+    pub fn run(&mut self, commands_string: String, input: String) {
+        self.parse_to_commands(commands_string);
+        self.get_input(input);
+        while self.command_pointer < self.commands.len() {
+            let command = self.commands[self.command_pointer];
+            match command {
+                Some(Commands::Forward) => self.forward(),
+                Some(Commands::Back) => self.back(),
+                Some(Commands::Increment) => self.increment(),
+                Some(Commands::Decrement) => self.decrement(),
+                Some(Commands::Input) => self.input(),
+                Some(Commands::Output) => self.output(),
+                Some(Commands::LoopStart) => self.loop_start(),
+                Some(Commands::LoopEnd) => self.loop_end(),
+                _ => (),
+            }
+            self.command_pointer += 1;
+        }
+    }
+
+    fn forward(&mut self) {
+        self.pointer = if self.pointer == self.size - 1 {
+            0
+        } else {
+            self.pointer + 1
+        }
+    }
+
+    fn back(&mut self) {
+        self.pointer = if self.pointer == 0 {
+            self.size - 1
+        } else {
+            self.pointer - 1
+        }
+    }
+
+    fn increment(&mut self) {
+        let value = self.memory[self.pointer];
+        self.memory[self.pointer] = value.wrapping_add(1)
+    }
+
+    fn decrement(&mut self) {
+        let value = self.memory[self.pointer];
+        self.memory[self.pointer] = value.wrapping_sub(1)
+    }
+
+    fn input(&mut self) {
+        let input_char = self.input.pop();
+        if input_char.is_some() {
+            let value = input_char.unwrap();
+            if value as u8 > 0 {
+                self.memory[self.pointer] = input_char.unwrap() as u8;
+            }
+        }
+    }
+
+    fn output(&mut self) {
+        let output_char = self.memory[self.pointer];
+        self.output.push(output_char as char)
+    }
+
+    fn skip_back(&mut self) {
+        let mut count: u8 = 1;
+        let mut op;
+        while count > 0 {
+            self.command_pointer -= 1;
+            op = self.commands[self.command_pointer];
+            match op {
+                Some(Commands::LoopStart) => count -= 1,
+                Some(Commands::LoopEnd) => count += 1,
+                _ => (),
+            }
+        }
+    }
+
+    fn skip_forward(&mut self) {
+        let mut count: u8 = 1;
+        let mut op;
+        while count > 0 {
+            self.command_pointer += 1;
+            op = self.commands[self.command_pointer];
+            match op {
+                Some(Commands::LoopStart) => count += 1,
+                Some(Commands::LoopEnd) => count -= 1,
+                _ => (),
+            }
+        }
+    }
+
+    fn loop_start(&mut self) {
+        if self.memory[self.pointer] == 0 {
+            self.skip_forward()
+        }
+    }
+
+    fn loop_end(&mut self) {
+        if self.memory[self.pointer] > 0 {
+            self.skip_back()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,172 +1,39 @@
-use std::iter;
+use clap::Parser;
+use std::fs;
+use std::process;
 
-#[derive(Clone, Debug, Copy)]
-pub enum Commands {
-    Forward,
-    Back,
-    Increment,
-    Decrement,
-    Output,
-    Input,
-    LoopStart,
-    LoopEnd,
-}
+mod lib;
 
-pub struct Interpreter {
-    memory: Vec<u8>,
-    size: usize,
-    commands: Vec<Option<Commands>>,
-    input: Vec<u8>,
-    output: Vec<u8>,
-    pointer: usize,
-    command_pointer: usize,
-}
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to program
+    #[clap(value_parser)]
+    program_path: String,
 
-impl Interpreter {
-    pub fn new(size: usize) -> Interpreter {
-        Interpreter {
-            memory: iter::repeat(0 as u8).take(size).collect(),
-            size: size,
-            commands: Vec::new(),
-            input: Vec::new(),
-            output: Vec::new(),
-            pointer: 0,
-            command_pointer: 0,
-        }
-    }
-
-    fn parse_to_commands(&mut self, commands_string: &str) -> () {
-        self.commands = commands_string
-            .chars()
-            .map(|ch| -> Option<Commands> {
-                match ch {
-                    '+' => Some(Commands::Increment),
-                    '-' => Some(Commands::Decrement),
-                    '>' => Some(Commands::Forward),
-                    '<' => Some(Commands::Back),
-                    '.' => Some(Commands::Output),
-                    ',' => Some(Commands::Input),
-                    '[' => Some(Commands::LoopStart),
-                    ']' => Some(Commands::LoopEnd),
-                    _ => None,
-                }
-            })
-            .collect();
-    }
-
-    fn get_input(&mut self, input: Vec<u8>) -> () {
-        self.input = input;
-        self.input.reverse();
-    }
-
-    pub fn get_output(self) -> Vec<u8> {
-        self.output
-    }
-
-    pub fn run(&mut self, commands_string: &str, input: Vec<u8>) {
-        self.parse_to_commands(commands_string);
-        self.get_input(input);
-        while self.command_pointer < self.commands.len() {
-            let command = self.commands[self.command_pointer];
-            match command {
-                Some(Commands::Forward) => self.forward(),
-                Some(Commands::Back) => self.back(),
-                Some(Commands::Increment) => self.increment(),
-                Some(Commands::Decrement) => self.decrement(),
-                Some(Commands::Input) => self.input(),
-                Some(Commands::Output) => self.output(),
-                Some(Commands::LoopStart) => self.loop_start(),
-                Some(Commands::LoopEnd) => self.loop_end(),
-                _ => (),
-            }
-            self.command_pointer += 1;
-        }
-    }
-
-    fn forward(&mut self) {
-        self.pointer = if self.pointer == self.size - 1 {
-            0
-        } else {
-            self.pointer + 1
-        }
-    }
-
-    fn back(&mut self) {
-        self.pointer = if self.pointer == 0 {
-            self.size - 1
-        } else {
-            self.pointer - 1
-        }
-    }
-
-    fn increment(&mut self) {
-        let value = self.memory[self.pointer];
-        self.memory[self.pointer] = value.wrapping_add(1)
-    }
-
-    fn decrement(&mut self) {
-        let value = self.memory[self.pointer];
-        self.memory[self.pointer] = value.wrapping_sub(1)
-    }
-
-    fn input(&mut self) {
-        let input_char = self.input.pop();
-        if input_char.is_some() {
-            let value = input_char.unwrap();
-            if value > 0 {
-                self.memory[self.pointer] = input_char.unwrap();
-            }
-        }
-    }
-
-    fn output(&mut self) {
-        let output_char = self.memory[self.pointer];
-        self.output.push(output_char)
-    }
-
-    fn skip_back(&mut self) {
-        let mut count: u8 = 1;
-        let mut op;
-        while count > 0 {
-            self.command_pointer -= 1;
-            op = self.commands[self.command_pointer];
-            match op {
-                Some(Commands::LoopStart) => count -= 1,
-                Some(Commands::LoopEnd) => count += 1,
-                _ => (),
-            }
-        }
-    }
-
-    fn skip_forward(&mut self) {
-        let mut count: u8 = 1;
-        let mut op;
-        while count > 0 {
-            self.command_pointer += 1;
-            op = self.commands[self.command_pointer];
-            match op {
-                Some(Commands::LoopStart) => count += 1,
-                Some(Commands::LoopEnd) => count -= 1,
-                _ => (),
-            }
-        }
-    }
-
-    fn loop_start(&mut self) {
-        if self.memory[self.pointer] == 0 {
-            self.skip_forward()
-        }
-    }
-
-    fn loop_end(&mut self) {
-        if self.memory[self.pointer] > 0 {
-            self.skip_back()
-        }
-    }
+    /// Path to input file
+    #[clap(short, long)]
+    input_path: Option<String>,
 }
 
 fn main() {
-    let mut interpreter = Interpreter::new(10);
-    interpreter.run(",>,>,.<.<.", "HEllo".to_string().as_bytes().to_vec());
-    println!("{:?}", interpreter.get_output());
+    let args = Args::parse();
+
+    let program = fs::read_to_string(args.program_path).unwrap_or_else(|err| {
+        eprintln!("Error reading program: {}", err);
+        process::exit(1);
+    });
+
+    let mut input = String::new();
+
+    if let Some(input_path) = args.input_path {
+        input = fs::read_to_string(input_path).unwrap_or_else(|err| {
+            eprintln!("Error parsing input: {}", err);
+            process::exit(1);
+        });
+    }
+
+    let mut interpreter = lib::Interpreter::new(2048);
+    interpreter.run(program, input);
+    println!("{}", interpreter.get_output());
 }
